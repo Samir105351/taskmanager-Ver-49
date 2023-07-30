6c4d2701df19ef713ccf745aae8900e5dce242be
Samir105351 <tahseenmubbarat1000@gmail.com>
2023-07-30 17:46:05 +0600

    Final Commit

--- src/main/resources/static/js/copy.js
+++ src/main/resources/static/js/copy.js
@@ -1,25 +1,28 @@
-function copyToClipboard(text, tooltip) {
-    const textarea = document.createElement('textarea');
-    textarea.value = text;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
-    showTooltip(tooltip);
-}
+    function copyToClipboard(text, tooltip) {
+        const textarea = document.createElement('textarea');
+        textarea.value = text;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+        showTooltip(tooltip);
+    }
 
-function showTooltip(tooltip) {
-    tooltip.classList.add('show');
-    setTimeout(() => {
-        tooltip.classList.remove('show');
-    }, 1500); // Show tooltip for 1.5 seconds
-}
+    function showTooltip(tooltip) {
+        tooltip.classList.add('show');
+        setTimeout(() => {
+            tooltip.classList.remove('show');
+        }, 1500); // Show tooltip for 1.5 seconds
+    }
 
-function copyListToClipboard() {
-    copyToClipboard(listText, document.querySelector('#tooltip-list'));
-}
+    function copyListToClipboard1(listText) {
+        copyToClipboard(listText1, document.querySelector('#tooltip-list1'));
+    }
+        function copyListToClipboard2(listText) {
+            copyToClipboard(listText2, document.querySelector('#tooltip-list2'));
+        }
 
-function copyPreToClipboard(preId, tooltipId) {
-    const preText = document.querySelector("#" + preId).innerText;
-    copyToClipboard(preText, document.querySelector('#' + tooltipId));
-}
+    function copyPreToClipboard(preId) {
+        const preText = document.querySelector("#" + preId).innerText;
+        copyToClipboard(preText, document.querySelector("#tooltip-list3"));
+    }
